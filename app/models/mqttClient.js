1bd73e0d8effd0888ed91cdbdbb0bec27407b576
Brian Lin <mebrianlin@gmail.com>
2016-03-04 18:11:37 -0800

    update dependency version to suppress error, add color support for frontend

--- app/models/mqttClient.js
+++ app/models/mqttClient.js
@@ -2,7 +2,7 @@ var mqtt = require('mqtt');
 
 module.exports = function() {
     var client;
-    var subscribeToTopic = '/etc_get';
+    var subscribeToTopic = '/etc_topic';
     var publishToTopic = '/etc_get';
 
     return {
@@ -29,11 +29,13 @@ module.exports = function() {
 
     function onMessage(topic, message) {
         // message is Buffer
-        console.log("topic: " + topic);
-        console.log("message: " + message);
+        // console.log("topic: " + topic);
+        // console.log("message: " + message);
+        if (parseInt(message) < 50)
+            client.publish(publishToTopic, "red");
+        else
+            client.publish(publishToTopic, "green");
         // client.end();
     }
 }
-
-
 
